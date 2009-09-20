00a291d6bc2ed63d0791a9774fafd5602c773aa6
Michael Nachbaur <mike@nachbaur.com>
2009-09-20 17:37:37 -0400

    Updated version number

--- lib/HTML/FormHandler/Render/Hash.pm
+++ lib/HTML/FormHandler/Render/Hash.pm
@@ -17,11 +17,11 @@ HTML::FormHandler::Render::Hash - render a form to a raw hash
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
